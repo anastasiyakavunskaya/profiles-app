1b5891f5b49f41678088799d2f93b3ea0199862d
anastasia <kavunskanas@gmail.com>
2020-11-05 21:56:04 +0300

    add intents to email and phone

--- app/src/main/java/com/example/profiles/details/DetailsFragment.kt
+++ app/src/main/java/com/example/profiles/details/DetailsFragment.kt
@@ -1,22 +1,23 @@
 package com.example.profiles.details
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.profiles.R
 import com.example.profiles.databinding.FragmentDetailsBinding
 import com.example.profiles.profiles.ProfileListener
-import com.example.profiles.profiles.ProfilesFragmentDirections
 import com.example.profiles.profiles.ProfilesListAdapter
-import com.example.profiles.profiles.ProfilesViewModel
-import androidx.lifecycle.Observer
-
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class DetailsFragment : Fragment() {
@@ -33,21 +34,43 @@ class DetailsFragment : Fragment() {
         binding.profile = profile
 
         val viewModel: DetailsViewModel =
-            ViewModelProvider(this, DetailsViewModel.Factory(activity.application, profile.friends))
+            ViewModelProvider(this, DetailsViewModel.Factory(activity.application, profile))
                 .get(DetailsViewModel::class.java)
 
+        binding.viewModel = viewModel
+        binding.email.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse("mailto:"+Uri.encode(profile.email))
+            requireContext().startActivity(
+                Intent.createChooser(
+                    emailIntent,"Send mail..."
+                )
+            )
+        }
+        binding.phone.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_CALL)
+            callIntent.data = Uri.parse("tel:"+Uri.encode(profile.email))
+            requireContext().startActivity(
+                Intent.createChooser(
+                    callIntent,"Call..."
+                )
+            )
+        }
 
 
         when(profile.eyeColor){
-            "brown" -> binding.eyeColorIndicator.setBackgroundColor(resources.getColor(R.color.colorBrown))
-            "green" -> binding.eyeColorIndicator.setBackgroundColor(resources.getColor(R.color.colorGreen))
-            "blue" -> binding.eyeColorIndicator.setBackgroundColor(resources.getColor(R.color.colorBlue))
+            "brown" -> binding.eyeColorIndicator.setImageResource(R.drawable.brown_eyes)
+            "green" -> binding.eyeColorIndicator.setImageResource(R.drawable.green_eyes)
+            "blue" -> binding.eyeColorIndicator.setImageResource(R.drawable.blue_eyes)
         }
         when(profile.favoriteFruit){
-            "banana" -> binding.fruitIndicator.setImageDrawable(resources.getDrawable(R.drawable.bananas))
-            "apple" -> binding.fruitIndicator.setImageDrawable(resources.getDrawable(R.drawable.apple))
-            "strawberry" -> binding.fruitIndicator.setImageDrawable(resources.getDrawable(R.drawable.strawberry))
+            "banana" -> binding.fruitIndicator.setImageResource(R.drawable.bananas)
+            "apple" -> binding.fruitIndicator.setImageResource(R.drawable.apple)
+            "strawberry" -> binding.fruitIndicator.setImageResource(R.drawable.strawberry)
         }
+
+        binding.registered.text = viewModel.formatDate()
+        binding.coordinates.text = viewModel.formatCoordinates()
 
         binding.lifecycleOwner = this
 
