69e769763992391d23bc15daa7854fc5ac66c0df
anastasia <kavunskanas@gmail.com>
2020-11-05 21:54:30 +0300

    add date and location converters

--- app/src/main/java/com/example/profiles/details/DetailsViewModel.kt
+++ app/src/main/java/com/example/profiles/details/DetailsViewModel.kt
@@ -1,26 +1,55 @@
 package com.example.profiles.details
 
 import android.app.Application
+import android.content.Intent
+import android.location.Location
+import android.location.Location.*
+import android.net.Uri
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.*
 import com.example.profiles.database.getDatabase
 import com.example.profiles.network.Friend
 import com.example.profiles.network.Profile
 import com.example.profiles.repository.ProfilesRepository
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-class DetailsViewModel(application: Application, friendsIds: MutableList<Friend>): AndroidViewModel(application) {
+class DetailsViewModel(application: Application, val profile: Profile): AndroidViewModel(application) {
 
     private val database = getDatabase(application)
     private val profilesRepository = ProfilesRepository(database)
-    val friends  = profilesRepository.getFriends(friendsIds)
+    val friends  = profilesRepository.getFriends(profile.friends)
+
+    fun formatDate(): String{
+        val date = profile.registered
+
+        var sdf = SimpleDateFormat("yyyy-mm-dd")
+        var stf = SimpleDateFormat("hh-mm")
+        val newDay: Date = sdf.parse(date)!!
+        val newTime: Date = stf.parse(date)!!
+        sdf = SimpleDateFormat("dd.mm.yy")
+        stf = SimpleDateFormat("hh:mm")
+        val result = stf.format(newTime) +" "+ sdf.format(newDay)
+        return "Registered: $result"
+    }
+
+    fun formatCoordinates(): String {
+        //широта
+        val lat = profile.latitude
+        //долгота
+        val lon = profile.longitude
+
+        return "Location: "+ Location.convert(lon, FORMAT_SECONDS) +" ; "+ Location.convert(lat, FORMAT_SECONDS)
+     }
 
 
-    class Factory(val app: Application,val  friends: MutableList<Friend>) : ViewModelProvider.Factory {
+    class Factory(val app: Application,val  profile: Profile) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(DetailsViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
-                return DetailsViewModel(app, friends) as T
+                return DetailsViewModel(app, profile) as T
             }
             throw IllegalArgumentException("Unable to construct viewmodel")
         }
